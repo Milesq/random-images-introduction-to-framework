fbc4412a06c42805fad6d622c47a6577249f7938
Milesq <wmilosz88@gmail.com>
2020-03-28 15:03:31 +0100

    App end

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,54 @@
-<main>Hello World!</main>
+<script>
+  import Controler from './components/Controller.svelte';
+  import { onMount } from 'svelte';
+  import Macy from 'macy';
+
+  onMount(() => {
+    new Macy({
+      container: '.macy-container',
+      margin: {
+        x: 20,
+        y: 30
+      },
+      columns: 6
+    });
+  });
+
+  let imgCount = 20;
+  $: images = new Array(imgCount).fill().map((el, i) => {
+    const w = Math.random() * 150 + 150;
+    const h = Math.random() * 100 + 250;
+
+    return [`https://picsum.photos/${Math.round(w)}/${Math.round(h)}`, i];
+  });
+</script>
+
+<main>
+  <div class="controller-container">
+    <div>Ile obrazków chcesz wyświetlić?</div>
+    <Controler bind:value="{imgCount}" />
+  </div>
+
+  <div class="macy-container">
+    {#each images as url (url[1])}
+      <img src="{url[0]}" alt="" />
+    {/each}
+  </div>
+</main>
+
+<style>
+  .controller-container {
+    display: flex;
+    padding: 50px;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .macy-container {
+    margin: 50px;
+  }
+
+  img {
+    user-select: none;
+  }
+</style>
---
+++ src/components/Controller.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let value = 0;
+  let inputMode = false;
+
+  const changeMode = () => (inputMode = !inputMode);
+  const dec = () => {
+    if (value > 0) {
+      --value;
+    }
+  };
+  const inc = () => ++value;
+
+  function blur() {
+    if (value < 0) {
+      value = Math.abs(value);
+    }
+  }
+</script>
+
+<div class="container">
+  <button on:click="{dec}">-</button>
+  <span on:dblclick="{changeMode}">
+    {#if inputMode}
+      <input type="number" on:blur="{blur}" min="0" bind:value />
+    {:else}
+      <span>{value}</span>
+    {/if}
+  </span>
+  <button on:click="{inc}">+</button>
+</div>
+
+<style>
+  .container {
+    color: white;
+  }
+</style>
